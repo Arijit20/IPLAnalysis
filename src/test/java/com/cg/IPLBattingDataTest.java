d097ceeb5bc4826bd59344e3510bb0fc44fe0ec6
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-27 02:51:37 +0530

    [Arijit]Added : Test Case Added For Top Batting Avg

--- src/test/java/com/cg/IPLBattingDataTest.java
+++ src/test/java/com/cg/IPLBattingDataTest.java
@@ -1,23 +1,39 @@
 package com.cg;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import org.junit.Before;
 import org.junit.Test;
-
-import com.cg.dto.CsvFileLoader;
 import com.cg.dto.IPLBatting;
 import com.cg.dto.IPLException;
+import com.cg.service.BattingDataSorter;
+import com.cg.service.IPLAnalyser;
 
 import junit.framework.Assert;
 
 public class IPLBattingDataTest {
 
-public static final String BOWLING_DATA_FILE_PATH = "D:/javaworkspace/IPLAnalysis/DataFiles/BattingData.csv";
+public static final String BATTING_DATA_FILE_PATH = "D:/javaworkspace/IPLAnalysis/DataFiles/BattingData.csv";
+private IPLAnalyser iplAnalyser;
+List<IPLBatting> battingList = new ArrayList<IPLBatting>();
+List<IPLBatting> sortedList;
+
+@Before
+public void initialize() throws IPLException {
+	iplAnalyser = new IPLAnalyser();
+	battingList = iplAnalyser.loadCsvFile(BATTING_DATA_FILE_PATH, IPLBatting.class);
+	sortedList = new ArrayList<IPLBatting>();
+}
 	
 	@Test
-	public void givenBowlingData_shouldRetuenList() throws IPLException {
-		CsvFileLoader csvFileLoader = new CsvFileLoader();
-		List<IPLBatting> battingList = csvFileLoader.loadCsvFile(BOWLING_DATA_FILE_PATH, IPLBatting.class);
+	public void givenBattingData_shouldRetuenList() throws IPLException {
 		Assert.assertEquals(101, battingList.size());
 	}
+	
+	@Test
+	public void givenBattingData_shouldReturnTopBattingAvg() {
+		sortedList = iplAnalyser.sortBattingData(battingList, BattingDataSorter.TOP_BATTING_AVG);
+		Assert.assertEquals("MS Dhoni", sortedList.get(100).getPlayer());
+	}
+	
 }
