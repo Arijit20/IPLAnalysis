10b94b2a82edc9383a338c2afa5b2c1f8b60871f
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-27 02:53:21 +0530

    [Arijit]Added : BattingSorter Class Created And Added Best Batting Avg Method

--- src/main/java/com/cg/service/IPLAnalyser.java
+++ src/main/java/com/cg/service/IPLAnalyser.java
@@ -1,5 +1,22 @@
 package com.cg.service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.cg.dto.CsvFileLoader;
+import com.cg.dto.IPLBatting;
+import com.cg.dto.IPLException;
+
 public class IPLAnalyser {
+	
+	BattingDataSorter battingDataSorter = new BattingDataSorter();
     
+	public <E> List<E> loadCsvFile(String csvFilePath, Class<E> csvClass) throws IPLException {
+		return new CsvFileLoader().loadCsvFile(csvFilePath, csvClass);
+	}
+	
+	public List<IPLBatting> sortBattingData(List<IPLBatting> battingList, Comparator<IPLBatting> comparator){
+		return (List<IPLBatting>) battingList.stream().sorted(comparator).collect(Collectors.toList());
+	}
 }
