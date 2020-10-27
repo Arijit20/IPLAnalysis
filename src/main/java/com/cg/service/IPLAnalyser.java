8b938970686cf81a231dc6713ca3db7fb209b6a9
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-27 19:02:14 +0530

    [Arijit]Added : Method For Comapring Best Batting And Bowling Averages

--- src/main/java/com/cg/service/IPLAnalyser.java
+++ src/main/java/com/cg/service/IPLAnalyser.java
@@ -1,17 +1,18 @@
 package com.cg.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.cg.dto.CsvFileLoader;
+import com.cg.dto.IPLAllRounder;
 import com.cg.dto.IPLBatting;
 import com.cg.dto.IPLBowling;
 import com.cg.dto.IPLException;
 
 public class IPLAnalyser {
-	
-	BattingDataSorter battingDataSorter = new BattingDataSorter();
     
 	public <E> List<E> loadCsvFile(String csvFilePath, Class<E> csvClass) throws IPLException {
 		return new CsvFileLoader().loadCsvFile(csvFilePath, csvClass);
@@ -21,7 +22,27 @@ public class IPLAnalyser {
 		return (List<IPLBatting>) battingList.stream().sorted(comparator).collect(Collectors.toList());
 	}
 	
-	public List<IPLBowling> sortBowlingData(List<IPLBowling> battingList, Comparator<IPLBowling> comparator){
-		return (List<IPLBowling>) battingList.stream().sorted(comparator).collect(Collectors.toList());
+	public List<IPLBowling> sortBowlingData(List<IPLBowling> bowlingList, Comparator<IPLBowling> comparator){
+		return (List<IPLBowling>) bowlingList.stream().sorted(comparator).collect(Collectors.toList());
+	}
+	
+	public List<IPLAllRounder> sortAllRounderData(List<IPLBatting> battingList, List<IPLBowling> bowlingList, Comparator<IPLAllRounder> comparator){
+		List<IPLAllRounder> allRounderList = getAllRounderPlayers( battingList, bowlingList);
+		Collections.reverse(allRounderList);
+		return (List<IPLAllRounder>) allRounderList.stream().sorted(comparator).collect(Collectors.toList());
+	}
+
+	private List<IPLAllRounder> getAllRounderPlayers(List<IPLBatting> battingList, List<IPLBowling> bowlingList) {
+		List<IPLAllRounder> allRounderList = new ArrayList<>();
+		battingList.stream().forEach(batsman -> {
+			IPLBowling bowlers = bowlingList.stream()
+					.filter(bowler -> bowler.getPlayer().equalsIgnoreCase(batsman.getPlayer())).findFirst()
+					.orElse(null);
+			if (bowlers != null) {
+				allRounderList.add(new IPLAllRounder(batsman.getPlayer(), batsman.getRuns(), bowlers.getNoOfWickets(),
+						batsman.getAverage(), bowlers.getAverage()));
+			}
+		});
+		return allRounderList;
 	}
 }
